3454d6043f028bdbfaf59e1b5a15cdeb33862777
repamungkas <pamungkas.re@gmail.com>
2019-10-18 08:31:10 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/bismillahuts/Activity/NavigationActivity.java
@@ -0,0 +1,16 @@
+package id.ac.polinema.bismillahuts.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.polinema.bismillahuts.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
